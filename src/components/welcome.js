a552eaa465f8dcb52713252c849b2019abde5eda
MohsenAzimi75 <mohsenazimi7525@gmail.com>
2023-10-10 21:16:50 +0330

    3 Commit

---
+++ src/components/welcome.js
@@ -0,0 +1,36 @@
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+
+const Welcome = () => {
+  const [showWelcome, setShowWelcome] = useState();
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("show-app-intro"));
+    setShowWelcome(JSON.parse(data) ?? true);
+  }, []);
+
+  const onHideWelcome = () => {
+    setShowWelcome(false);
+    localStorage.setItem("show-app-intro", JSON.stringify(false));
+  };
+
+  return (
+    <React.Fragment>
+      {showWelcome && (
+        <div className="container">
+          <div className=" bg-primary text-white my-3">
+            <FontAwesomeIcon
+              icon={faClose}
+              style={{ float: "right", margin: "5px" }}
+              onClick={onHideWelcome}
+            />
+            <div className="p-4">welcome</div>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Welcome;
